0ce29150bbe02db5810c1e51479230af795daddc
saranga pani sarma <sarangapani@siqes.com>
2025-02-19 11:19:19 +0530

    fcm added in ticket

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -6,7 +6,7 @@ use App\Providers\RouteServiceProvider;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-
+use Session;
 class RedirectIfAuthenticated
 {
     /**
@@ -19,6 +19,8 @@ class RedirectIfAuthenticated
      */
     public function handle(Request $request, Closure $next, ...$guards)
     {
+        Session::put('fcm_token', $request->fcm_token);
+
         $guards = empty($guards) ? [null] : $guards;
 
         foreach ($guards as $guard) {
